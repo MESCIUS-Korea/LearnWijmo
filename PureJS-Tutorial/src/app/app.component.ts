7fc7e423f2b2e4f474cea65a5f0a3d3a4c0076c8
DylanHwang <hdg8902@gmail.com>
2017-02-16 16:59:42 +0900

    02/16 - json 리스트 분리 - Dylan
    02/16 - json 리스트 분리 - Dylan

--- PureJS-Tutorial/src/app/app.component.ts
+++ PureJS-Tutorial/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Router, RouterModule} from '@angular/router';
+import { JsonDataService } from './json-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [JsonDataService]
 })
 export class AppComponent {
-  title = 'app works!';
+
+ constructor(private router: Router, private jsonDataService: JsonDataService) { }
+
+  toc = [];
+
+  onSelect(url: string) {
+      console.log(url);
+    this.router.navigate([url]);
+  }
+
+  ngOnInit(){
+      this.jsonDataService.fatchData().subscribe(
+        (data) => this.toc = data
+      );
+  }
+
+  
+
+
+
 }
--- PureJS-Tutorial/src/app/app.module.ts
+++ PureJS-Tutorial/src/app/app.module.ts
@@ -5,16 +5,34 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ReferencingComponent } from './referencing/referencing.component';
-import { OverviewComponent } from './overview/overview.component';
+import { ReferencingComponent } from './wijmo5-Info/referencing/referencing.component';
+import { OverviewComponent } from './wijmo5-Info/overview/overview.component';
 import { FilterComponent } from './flexgrid/filter/filter.component';
+import { Ng1DirectiveComponent } from './wijmo5-Info/ng1-directive/ng1-directive.component';
+import { SizingStyleComponent } from './wijmo5-Info/sizing-style/sizing-style.component';
+import { PsesudoComponent } from './wijmo5-Info/psesudo/psesudo.component';
+import { EventsComponent } from './wijmo5-Info/events/events.component';
+import { EnumerationsComponent } from './wijmo5-Info/enumerations/enumerations.component';
+import { GlobalizationComponent } from './wijmo5-Info/globalization/globalization.component';
+import { GlyphsComponent } from './wijmo5-Info/glyphs/glyphs.component';
+import { IntellisenseComponent } from './wijmo5-Info/intellisense/intellisense.component';
+import { LesscssComponent } from './wijmo5-Info/lesscss/lesscss.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReferencingComponent,
     OverviewComponent,
-    FilterComponent
+    FilterComponent,
+    Ng1DirectiveComponent,
+    SizingStyleComponent,
+    PsesudoComponent,
+    EventsComponent,
+    EnumerationsComponent,
+    GlobalizationComponent,
+    GlyphsComponent,
+    IntellisenseComponent,
+    LesscssComponent
   ],
   imports: [
     BrowserModule,
